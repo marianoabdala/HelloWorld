5ccb1c0cf0028efb0d410f0dc4eca114150f132d
Mariano Abdala <mariano@zerously.com>
2018-03-21 13:03:24 -0300

    Testing failure

--- helloworld/helloworldTests/helloworldTests.swift
+++ helloworld/helloworldTests/helloworldTests.swift
@@ -24,6 +24,8 @@ class HelloworldTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        XCTFail("TESTING FAILURE!")
     }
     
     func testPerformanceExample() {
